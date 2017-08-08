d9fb6813043195b4655165442e2ba18b464eb1ad
frankstar123 <andrew.howard@opencastsoftware.com>
2017-08-08 09:26:46 +0100

    Changes to trigger against ic_brcledger

--- bdx/triggers/trigger_brcledger.sql
+++ bdx/triggers/trigger_brcledger.sql
@@ -1,10 +1,9 @@
-
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TRIGGER [dbo].[trig_brcledger_create] on [dbo].[brcledger] 
-instead of INSERT as 
+CREATE TRIGGER [dbo].[trig_brcledger] on [dbo].[ic_brcledger]
+instead of insert as
 BEGIN
 	DECLARE @Branch int;
 	DECLARE @PK varchar(28);
@@ -24,18 +23,19 @@ BEGIN
 
 	DECLARE c_inserted cursor for
 		                     --SELECT Branch@,[Key@],[ClientRef@], [PolicyRef@],[Suffix@],[Poltype#],[Trantype#], [Dt_raised] from INSERTED
-					SELECT B@,[Key@],[Ref@], [PolRef@],[Suffix@],[Poltype#],[Trantype#],[Dt_raised#], [Insurer#] , [Orig_debt#], [Comm_amt#],
-					[Ipt_rate#],[ipt_amount#],[yoa#],[Agent_comm#] from INSERTED
+					SELECT B@,[Key@],[Ref@], [PolRef@],[Suffix@],[Poltype],[Trantype],[Dt_raised], [Insurer] , [Orig_debt], [Comm_amt],
+					[Ipt_rate],[ipt_amount],[yoa],[Agent_comm] from INSERTED
 					
 	open c_inserted
 	fetch next from c_inserted into @Branch, @PK, @ClientRef, @PolicyId, @Suffix, @PolicyType, @TransType, @DateRaised,@Insurer,@OriginalDebt,
 									@CommissionAmount,@IptRate,@IptAmount,@YearOfAccount,@AgentCommission
 	while @@FETCH_STATUS <> -1
 	begin
-		exec dbo.bdxManagement @Branch, @PK, @ClientRef, @PolicyId,@Suffix, @PolicyType, @TransType, @DateRaised, 'Create';
+		exec dbo.bdxManagement @Branch, @PK, @ClientRef, @PolicyId,@Suffix, @PolicyType, @TransType, @DateRaised, 'Create',@YearOfAccount;
 		fetch next from c_inserted into @Branch, @PK, @ClientRef, @PolicyId, @Suffix, @PolicyType, @TransType, @DateRaised,@Insurer,@OriginalDebt,@CommissionAmount,@IptRate,@IptAmount,@YearOfAccount,@AgentCommission
 		
 	end
 	close c_inserted;
 	deallocate c_inserted;
 END;
+
