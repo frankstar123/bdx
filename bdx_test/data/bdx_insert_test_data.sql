9d7577c021cb87ed38ae7e8a5c4732da075c7735
frankstar123 <andrew.howard@opencastsoftware.com>
2017-09-05 15:31:23 +0100

    Added Test Data file

--- bdx_test/data/bdx_insert_test_data.sql
+++ bdx_test/data/bdx_insert_test_data.sql
@@ -1,6 +1,8 @@
 
 DECLARE @FK int =0;
 
+DELETE FROM rep_bdx_riskdata where PolicyRef = 'HOWA01HN01';
+
 INSERT INTO [dbo].[rep_bdx_ledger] ( [timestamp],[method],[version],[branch],[key],[clientRef],[policyRef],[suffix],[policyType],[transType],[dateRaised],[processed])
      VALUES
 (GETDATE(), 'Create',null,0,'TESTDATA','HOAW01','HOAW01HN01',1,'HN','New Business',GETDATE(), 0);
@@ -21,3 +23,6 @@ INSERT INTO [dbo].[rep_bdx_riskdata]
 	(@FK,0,'HOWA01','HOWA01HN01',2,'Andrew Howard','ABC12345','Catlin HNW', 'HN', 3, GETDATE(), '01/08/2017', '01/09/2017','Create','Endorsement','16 Ringway, NE62 5XT', 	'NE62 5XT', 2017,2500000,2500,100000,200,5000,25,20000,175,0,0,0,0,0,0,0,0),	
 	(@FK,0,'HOWA01','HOWA01HN01',3,'Andrew Howard','ABC12345','Catlin HNW', 'HN', 3, GETDATE(), '01/08/2017', '01/09/2017','Create','Endorsement','44 Gairloch Drive', 	'NE38 0DS', 	2017,2000000,1250,100000,200,5000,25,20000,175,0,0,0,0,0,0,0,0)
 GO
+
+
+
